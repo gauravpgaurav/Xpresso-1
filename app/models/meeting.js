35c5fde2bfd6d4b26420fb83ef553de78cefdd07
Gaurav Pant <gauravpgaurav@gmail.com>
2017-12-03 15:08:16 -0600

    Meeting model updated to store Watson response

--- app/models/meeting.js
+++ app/models/meeting.js
@@ -8,6 +8,21 @@ module.exports = mongoose.model('Meeting', {
 	Start_time: {type: String},
 	End_time: {type: String},
 	Duration: {type:String},
-	Topics: [{ Serial_No: String,
-				Topic_Name: String }]
+	Topics: [
+        { Serial_No: String,
+				Topic_Name: String }
+    ],Undiscussed_Topics: [
+        { type: String}
+    ],Discussed_Topics: [
+        { type: String}
+    ],
+    Transcription: [{
+        Timestamps:[{type: Number}, {type: Number}],
+        Text: {type: String},
+        Result_Index: {type: Number}
+    }],
+    Speakers: [{
+        Timestamps:[{type: Number}, {type: Number}],
+        Speaker_Index: {type: Number}
+    }]
 });
