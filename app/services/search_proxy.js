7ba9f277c9f474ee25850da61ca26832b91a829c
shail <srakhu2@uic.edu>
2017-12-03 00:35:01 -0600

    Small fix in Bing search proxy and removing logs

--- app/services/search_proxy.js
+++ app/services/search_proxy.js
@@ -5,8 +5,6 @@ function getBingResult(query, callback) {
   var bing = "http://www.bing.com";
   var query = "?q=" + query;
   request(bing + query, function(err, response, body) {
-    console.log(err);
-    console.log(response.statusCode);
     if(response && response.statusCode != 200) {
       callback(err, response, {});
     } else {
@@ -21,7 +19,7 @@ function scrapeFirstResultOfBingPage(html) {
   var out = {};
   out.link = $('#b_results .b_algo a').attr('href');
   out.title = $('#b_results .b_algo a').html();
-  out.desc = $('#b_results .b_algo .b_caption .b_snippet p').html();
+  out.desc = $('#b_results .b_algo .b_caption p').html();
 
   return out;
 }
